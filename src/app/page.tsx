36e5bb9d339679e9fd1a096ca94e06a263908c57
Your Name <wiktorgs98@gmail.com>
2024-09-19 12:36:57 +0200

    convex data base working connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,25 @@
-
+'use client'
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
-import {  currentUser } from "@clerk/nextjs/server";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { SignInButton, SignOutButton,SignedIn,SignedOut } from "@clerk/nextjs";
+// import {  currentUser } from "@clerk/nextjs/server";
+// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 
 export default async function Home() {
 
-  const user = await currentUser()
 
-  console.log(user?.firstName)
-
-  if(!user) return <SignInButton  mode="modal"><Button>Sing in</Button></SignInButton>
-
+  const createFile = useMutation(api.files.createFile)
   
 
   return (
     <div className="flex flex-col gap-y-40 items-center">
-      <div className="flex gap-6 w-full justify-around">
-        <Button variant={"default"}>test</Button>
-        <SignOutButton><Button variant={"destructive"} >Sign out</Button></SignOutButton>
-      </div>
 
-      <h1 className="max-w-max p-8 bg-cyan-200 text-black flex items-center gap-x-4 rounded-xl opacity-75 ">
-        <Avatar>
-            <AvatarImage src={`${user?.imageUrl}`} />
-            <AvatarFallback>WG</AvatarFallback>
-        </Avatar>
-        <p>{`${user?.firstName}`}</p>
-      </h1>
-      
+      <SignedIn><SignOutButton><Button variant={"destructive"}>Sign Out</Button></SignOutButton></SignedIn>
+      <SignedOut><SignInButton mode="modal"><Button variant={"default"}>Sign In</Button></SignInButton></SignedOut>
+      <Button onClick={()=>{createFile({name:'hello'})}}>add to database</Button>
+
     </div>
   );
 }
