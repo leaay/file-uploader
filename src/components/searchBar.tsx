2de39b37905551544334a82e1fc0040c7edd2b7b
Your Name <wiktorgs98@gmail.com>
2024-10-18 17:27:35 +0200

    fix

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form"
+import {Form,FormControl,FormField,FormItem} from "@/components/ui/form"
 import { Input } from "./ui/input";
 import { Search } from "lucide-react";
 
