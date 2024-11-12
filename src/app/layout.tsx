3c8a8c6d00d8f2d4a0207b0f7ab113efd26ea481
Your Name <wiktorgs98@gmail.com>
2024-11-12 13:27:54 +0100

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,12 @@ export default function RootLayout({
         <meta property="og:title" content="Uploaderv1" />
         <meta property="og:description" content="Upload, manage, and securely store your files in one place." />
         <meta property="og:url" content="https://uploaderv1.netlify.app" />
-        <meta property="og:image" content="/androind-chrome-192x192.png" />
+        <meta property="og:image" content="/homepagepreview.png" />
 
-        <meta name="twitter:card" content="images/androind-chrome-192x192.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Uploaderv1" />
         <meta name="twitter:description" content="Upload, manage, and securely store your files in one place." />
-        <meta name="twitter:image" content="/androind-chrome-192x192.png" />
+        <meta name="twitter:image" content="/homepagepreview.png" />
 
       </Head>
       <body className="min-h-screen flex flex-col ">
